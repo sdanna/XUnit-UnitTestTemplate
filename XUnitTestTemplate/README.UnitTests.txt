b645b40a1475f6a408f1456c7d6293601ef20b0f
Steven D'Anna <steven.danna@gmail.com>
2013-12-23 14:09:52 -0600

    Initial Commit

---
+++ XUnitTestTemplate/README.UnitTests.txt
@@ -0,0 +1,65 @@
+﻿The namespace for true unit tests should follow the pattern
+of just being the name of the class under test.  Set the 
+folders to not be a namespace provider.  Use the folder 
+structure of the project that the class under tests uses in 
+its project.  
+
+Example:
+Suppose you have a project called WebProject with a folder 
+called Components with a class called Emailer that you want
+to test.
+
+WebProject
+     |___Components
+            |____Emailer.cs
+            
+Your corresponding XUnit test project would have a structure
+like the following.
+
+XUnitProject
+     |___Components
+            |____EmailerTests.cs
+            
+Look at this class for an example of how to set up the nested 
+namespacing so that tests are well organized and also at how 
+they should be named.
+
+namespace ExampleClassTests
+{
+    public class MethodUnderTest1_Should
+    {
+        [Fact]
+        public void Return_SomeValue_Under_Some_Condition()
+        {
+            // Arrange, Act, Assert
+        }
+
+        [Fact]
+        public void Return_SomeOtherValue_Under_Another_Condition()
+        {
+            // Arrange, Act, Assert
+        }
+    }
+
+    public class MethodUnderTest2_Should
+    {
+        [Fact]
+        public void Return_SomeValue_Under_Some_Condition()
+        {
+            // Arrange, Act, Assert
+        }
+
+        [Fact]
+        public void Throw_A_Specific_Type_Of_Exception_Under_Some_Condition()
+        {
+            // Arrange, Act, Assert
+        }
+    }
+}
+
+See the XUnit_UnitTestProjectReference.cs class for some best practices as 
+far as test layout goes.  
+
+Definitely look at the links provided in the comments at the top of the file
+to get a handle on how the framework works.  Have a good look at how to 
+setup and teardown tests as it is different in XUnit than in other frameworks.
---
+++ XUnitTestTemplate/XUnit_UnitTestProjectReference.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Extensions;
+using Xunit.Should;
+
+// XUnit - Unit Test Template and base capabilities
+namespace ClassUnderTest
+{
+    /// <summary>
+    /// XUnit Reference - Base library
+    /// http://xunit.codeplex.com/wikipage?title=Comparisons
+    /// 
+    /// Extensions Reference - Provides Data driven Theory attributes
+    /// http://blog.benhall.me.uk/2008/01/introduction-to-xunit-net-extensions/
+    /// 
+    /// Should Reference - Provides a fluent interface for assertions
+    /// https://github.com/enkari/xunit.should/blob/master/src/xunit.should/ShouldExtensions.cs
+    /// </summary>
+    public class ConcatenateStrings_Should
+    {
+        [Fact] // 1.  Decorate all non data driven tests with the [Fact] attribute
+        public void Return_The_Concatenation_Of_Two_Strings_When_Both_Strings_Arent_Null()
+        {
+            // Arrange // 2. Use the Arrange, Act, Assert pattern.
+            var classUnderTest = new ClassUnderTest();
+            var string1 = "unit";
+            var string2 = "test";
+
+            // Act
+            var actualValue = classUnderTest.ConcatenateStrings(string1, string2);
+
+            // Assert // 3.  Use the fluent "should" methods to write readable assertions about the results.
+            actualValue.ShouldBe("unittest");
+        }
+
+        [Fact]
+        public void Throw_An_Argument_Exception_When_A_Is_Null()
+        {
+            // Arrange
+            var classUnderTest = new ClassUnderTest();
+            string string1 = null;
+            string string2 = "test";
+
+            // 4. When checking exceptions the Act step occurs within the Assert.
+            // Act, Assert
+            Assert.Throws<ArgumentException>(() =>
+                {
+                    var actualValue = classUnderTest.ConcatenateStrings(string1, string2);
+                });
+        }
+
+        /*
+         *  5.  Use the [Theory] attribute for data driven tests.  
+         *  This will allow you to provide inputs to your test method.
+         *  The test will be run once for each input.  You can provide 
+         *  lots of different sources.  See the Extensions reference at 
+         *  the top of this file for more information.
+         */
+        [Theory]
+        [InlineData(null, "test")]
+        [InlineData("unit", null)]
+        public void Throw_An_Argument_Excpetion_When_Either_Input_Is_Null(string string1, string string2)
+        {
+            // Arrange
+            var classUnderTest = new ClassUnderTest();
+
+            // Act, Assert
+            Assert.Throws<ArgumentException>(() =>
+            {
+                var actualValue = classUnderTest.ConcatenateStrings(string1, string2);
+            });
+        }
+    }
+
+    // 6. Each method under test should live in it's own class.  It
+    // Makes the output in the test runner much easier to read.
+    public class Add_Should
+    {
+        [Fact]
+        public void Return_Its_Inputs_Added_Together()
+        {
+            // Arrange
+            var classUnderTest = new ClassUnderTest();
+
+            // Act
+            var actualValue = classUnderTest.AddIntegers(1, 2);
+
+            // Assert
+            actualValue.ShouldBe(3);
+        }
+    }
+
+
+
+
+
+    public class ClassUnderTest
+    {
+        public string ConcatenateStrings(string a, string b)
+        {
+            if (a == null) throw new ArgumentException("a must be a non null string instance.", "a");
+            if (b == null) throw new ArgumentException("b must be a non null string instance.", "b");
+
+            return a + b;
+        }
+
+        public int AddIntegers(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
